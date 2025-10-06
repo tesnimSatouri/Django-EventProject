fde203d59f764de62022ffc3b395dcab9f44d3c8
tesnimSatouri <tesnim.satouri@esprit.tn>
2025-10-06 13:22:30 +0100

    Implement event listing view and template, and update URL routing

--- Evenement/views.py
+++ Evenement/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Event
 # Create your views here.
+def listEvent(request):
+    list=Event.objects.all()
+    return render(request,'Event/list.html',{'ll':list})
--- EventProject/urls.py
+++ EventProject/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Person/',include('Person.urls') )
+    path('Person/',include('Person.urls') ),
+    path('Evenement/',include('Evenement.urls') )
 ]
